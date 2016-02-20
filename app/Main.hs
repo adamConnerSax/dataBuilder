df771e205fa816a779ed6f06f4e51bf86fcc8abf
adamConnerSax <adam_conner_sax@yahoo.com>
2016-02-20 00:25:42 -0500

    interim...
    DataBuilderTH.hs compiles but once spliced into main things are a mess…

--- app/Main.hs
+++ app/Main.hs
@@ -2,6 +2,7 @@
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE DeriveDataTypeable #-}
 {-# LANGUAGE NoMonomorphismRestriction #-} --just for demo.  Allows polymorphic result printer
 module Main where
 
@@ -10,16 +11,17 @@ import Control.Monad (ap)
 import Language.Haskell.TH
 --import Language.Haskell.TH.Lift
 import Data.Functor.Identity
-import Data.List (intercalate)
+import Data.List (intercalate,find)
 import Text.Read (readMaybe)
 import System.IO (hFlush,stdout)
+import Data.Data (Data)
 
-data TestNull = Null deriving (Show)
-data TestOne = One Int deriving (Show)
-data TestTwo = Two Int String deriving (Show)
-data TestSum = A | B Int | C Char Int | D Char Int Bool deriving (Show)
-data TestRecord = TestR { intF::Int, stringF::String } deriving (Show)
-data TestNested = Nested Int String TestSum deriving (Show)
+data TestNull = Null deriving (Show,Data)
+data TestOne = One Int deriving (Show,Data)
+data TestTwo = Two Int String deriving (Show,Data)
+data TestSum = A | B Int | C Char Int | D Char Int Bool deriving (Show,Data)
+data TestRecord = TestR { intF::Int, stringF::String } deriving (Show,Data)
+data TestNested = Nested Int String TestSum deriving (Show,Data)
 
 deriveBuild ''TestNull
 deriveBuild ''TestOne
@@ -28,6 +30,66 @@ deriveBuild ''TestSum
 deriveBuild ''TestRecord
 deriveBuild ''TestNested
 
+data BuilderEx a = BuilderEx (Maybe Metadata) (IO (Maybe a))
+
+builtIO::BuilderEx a -> IO (Maybe a)
+builtIO (BuilderEx _ ioma) = ioma
+
+sumBEs::[BuilderEx a]->BuilderEx a
+sumBEs bes = case (length bes) of
+  0 -> putStrLn "No constructors in sumBEs" >> return Nothing
+  1 -> head bes
+  _ -> do
+    let conName (BuilderEx (Metadata _ con _)) = con
+        toName be = conName be ++ if not bIsNothing be then "*" else ""
+        names = intercalate "," $ map toName bes
+        prompt = "Type has multiple constructors. Please choose (" ++ names ++ "): "
+    putStr prompt
+    hFlush stdout
+    cid <- getLine
+    let mBe = find (\be->conName be == cid) bes
+    case mBe of
+      Nothing -> putStrLn (cid ++ " unrecognized constructor!") >> return Nothing
+      Just be -> be
+
+instance Buildable BuilderEx where
+  bInject md x = BuilderEx md (return (Just x))
+  bApply (BuilderEx mAB iomAB) (BuilderEx mA iomA) = BuilderEx mAB iomB where
+    iomB = do
+      mf <- iomAB
+      ma <- iomA
+      return $ mf <*> ma
+  bNothing = BuilderEx Nothing Nothing
+  bSum = sumBEs
+  bMeta (BuilderEx md _) = md
+  bIsNothing (BuilderEx _ ma) = (ma == return Nothing) 
+   
+
+-- You need base case builders.  How to build basic types
+simpleBuilder::(Show a, Read a, Data a)=>Maybe FieldName->Maybe a->BuilderEx a 
+simpleBuilder mFN Nothing = BuilderEx (gatherMetaData undefined::a) (putStr "Enter: " >> hFlush stdout >> ((readMaybe <$> getLine)))
+simpleBuilder mFN (Just a) = BuilderEx (gatherMetaData undefined::a) (putStr ("Enter (was=" ++ show a ++ "):") >> hFlush stdout >> ((readMaybe <$> getLine)))
+
+-- This is overlappable so that we can use the TH to derive this for things.  Otherwise this covers all things!
+instance {-# OVERLAPPABLE #-} (Show a, Read a,Data a)=>Builder BuilderEx a where
+  buildM = simpleBuilder 
+
+f = maybe (putStrLn "built Nothing") (putStrLn . show) 
+
+
+main::IO ()
+main = do
+
+  putStrLn "Given:\ndata TestSum = A | B Int | C Char Int | D Char Int Bool deriving (Show)\ndata TestNested = Nested Int String TestSum deriving (Show)"
+  putStrLn "Build a TestSum from a given value (=C 'a' 12)"
+  builtIO (buildM Nothing (Just $ C 'a' 12)) >>= f
+  putStrLn "Build a TestNested from Nothing"
+  builtIO (buildM Nothing (Nothing :: Maybe TestNested)) >>= f
+  putStrLn "Build a TestSum from a value (=TestNested 12 \"Hello\" (D 'a' 2 True)"
+  builtIO (buildM Nothing (Just $ Nested 12 "Hello" (D 'a' 2 True))) >>= f
+  
+
+{-
 -- You need base case builders.  How to build basic types
 simpleBuilder::(Show a, Read a)=>Maybe a->MFM IO Identity a 
 simpleBuilder Nothing = putStr "Enter: " >> hFlush stdout >> ((fmap Identity) (readMaybe <$> getLine))
@@ -53,17 +115,4 @@ instance ApplicativeLike f=>ConSummable IO f where
         Nothing -> putStrLn (cid ++ " unrecognized constructor!") >> return (alInject Nothing)
         Just (SumConstructor _ b) -> b
 
-
-f = maybe (putStrLn "built Nothing") (putStrLn . show) 
-
-main::IO ()
-main = do
-
-  putStrLn "Given:\ndata TestSum = A | B Int | C Char Int | D Char Int Bool deriving (Show)\ndata TestNested = Nested Int String TestSum deriving (Show)"
-  putStrLn "Build a TestSum from a given value (=C 'a' 12)"
-  runIdentity <$> buildM (Just $ C 'a' 12) >>= f
-  putStrLn "Build a TestNested from Nothing"
-  runIdentity <$> buildM (Nothing :: Maybe TestNested) >>= f
-  putStrLn "Build a TestSum from a value (=TestNested 12 \"Hello\" (D 'a' 2 True)"
-  runIdentity <$> buildM (Just $ Nested 12 "Hello" (D 'a' 2 True)) >>= f
-  
+-}
--- lib/DataBuilderTH.hs
+++ lib/DataBuilderTH.hs
@@ -16,52 +16,80 @@ import qualified Data.Map as M
 import Data.Char (toLower)
 import Control.Exception (throwIO,Exception)
 import Data.Typeable (Typeable)
-import Data.Data (toConstr,dataTypeName,dataTypeOf)
+import Data.Data (toConstr,dataTypeName,dataTypeOf,Data)
 
 data BuilderException = BuilderException String deriving (Show,Typeable)
 instance Exception BuilderException
 
-data Metadata = Metadata { typeName::String, conName::Maybe String, fieldNames::[String] } 
+type TypeName = String
+type FieldName = String
+type ConName = String
+data Metadata = Metadata { typeName::TypeName, conName::ConName, fieldName::Maybe FieldName} 
 
-gatherMetaData::Data a=>a->MetaData
-gatherMetaData a = let c = toConstr a in MetaData (dataTypeName $ dataTypeOf a) (show c) (constrFields c)
+gatherMetaData::Data a=>a->Metadata
+gatherMetaData a = let c = toConstr a in Metadata (dataTypeName $ dataTypeOf a) (show c) Nothing
 
---type ConId = String
+type ConId = String
 --data SumConstructor m f a = SumConstructor { dflt::Bool, builder::MFM m f a }
 
 
 class Buildable f where
   -- inject and apply are exactly Applicative, inject=pure and apply=(<*>)
-  bInject::a -> f a
+  bInject::Metadata-> a -> f a
   bApply::f (a->b) -> f a -> f b
   -- nothing and sum are *close* to MonadPlus, nothing = mzero and sum ~ foldl mplus nothing but we need to combine all at once
   bNothing::f a
   bSum::[f a]->f a
-  -- this is problematic.  How do we fill this in for the base types? We can do this with Data.Data and "toConstr" and "dataTypeName . dataTypeOf" 
   bMeta::f a -> Metadata
   bIsNothing::f a -> Bool
 
 {-- An example
 
-data BuilderEx a = BuilderEx (Maybe MetaData) (IO (Maybe a)) deriving (Data)
+data BuilderEx a = BuilderEx (Maybe MetaData) (IO (Maybe a)) 
+
+sumBEs::[BuilderEx a]->BuilderEx a
+sumBEs bes = case (length bes) of
+  0 -> PutStrLn "No constructors in sumBEs" >> return Nothing
+  1 -> head bes
+  _ -> do
+    let conName (BuilderEx (Metadata _ con _)) = con
+        toName be = conName be ++ if not bIsnothing be then "*" else ""
+        names = intercalate "," $ map toName bes
+        prompt = "Type has multiple constructors. Please choose (" ++ names ++ "): "
+    putStr prompt
+    hFlush stdout
+    cid <- getLine
+    let mBe = find (\be->conName be == cid) bes
+      case mBe of
+        Nothing -> putStrLn (cid ++ " unrecognized constructor!") >> return Nothing
+        Just be -> be
 
 instance Buildable BuilderEx where
-  bInject x = BuilderEx (gatherMetaData x) (return (Just x))
-  bApply fAB fA = do
-   mf <- fAB
-   ma <- fA
+  bInject md x = BuilderEx md (return (Just x))
+  bApply (BuilderEx mAB iomAB) (BuilderEx mA iomA) = Builder mAB iomB where
+    iomB = do
+      mf <- iomAB
+      ma <- iomA
+      return $ mf <*> ma
+  bNothing = BuilderEx Nothing Nothing
+  bSum = sumBEs
+  bMeta (BuilderEx md _) = md
+  bIsNothing (BuilderEx _ ma) = (ma == return Nothing) ??
    
 -}
 
+--String is field name, if present, in enclosing data type.
 class Builder f a where
-  buildM::Buildable f=>a -> f a
+  buildM::Buildable f=>Maybe FieldName->Maybe a-> f a
 
 unsupported :: String -> Q a
 unsupported msg = fail ("Unsupported type: " ++ show msg)
 
-conNameAndTypes::Con->Q (Name,[Type])
-conNameAndTypes (NormalC n ts) = return (n, snd <$> ts)
-conNameAndTypes (RecC n vts) = let thrd (_,_,x) = x  in return (n,thrd <$> vts)
+conNameAndTypes::Con->Q (Name,[Type],Maybe [Name])
+conNameAndTypes (NormalC n ts) = return (n, snd <$> ts,Nothing)
+conNameAndTypes (RecC n vts) =
+  let thrd (_,_,x) = x
+      first (x,_,_) = x in return (n,thrd <$> vts, Just (first <$> vts))
 conNameAndTypes c = unsupported ("constructor type in conNameAndTypes (" ++ show c ++ ")")
 
 getCons (DataD _ _ _ c _) = return c
@@ -79,74 +107,91 @@ lookupType n = do
 
 deriveBuild::Name -> Q [Dec]
 deriveBuild name = do
-  [d|instance Builder IO Identity $(conT name) where
-       buildM Nothing  = $(handleNothingL name)
-       buildM (Just x) = $(handleJustL name) x|]
+  [d|instance Buildable f=>Builder f $(conT name) where
+       buildM mFn Nothing  = $(handleNothingL name) mFn
+       buildM mFn (Just x) = $(handleJustL name) mFn x|]
 
 handleNothingL::Name->Q Exp
 handleNothingL n = do
+  mFnN <- newName "mFn"
+  let patsQ = [return $ VarP mFnN]
+      mFnE = VarE mFnN
+  lamE patsQ (handleNothing n mFnE)
+
+handleNothing::Name->Exp->Q Exp
+handleNothing n mFnE = do
   sumFE <- [e|\x -> bSum x|]
-  blankBuildersMap <- buildAllBlankBuilders n
-  let summedE = sumFE `AppE` (ListE $ map (\(cid,bldrE) -> TupE [LitE $ StringL cid,bldrE]) (M.toList blankBuildersMap))
+  blankBuildersMap <- buildAllBlankBuilders n mFnE
+  let summedE = sumFE `AppE` (ListE . snd . unzip . M.toList $ blankBuildersMap)
   return $ summedE
 
 handleJustL::Name->Q Exp
 handleJustL n = do
+  mFnN <- newName "mFn"
   aN <- newName "a"
-  let patQ = return $ VarP aN
+  let patsQ = return <$> [VarP mFnN, VarP aN]
+      mFnE = VarE mFnN        
       aE = VarE aN
-  lamE [patQ] (handleJust aE n) 
+  lamE patsQ (handleJust n mFnE aE) 
 
-handleJust::Exp->Name->Q Exp
-handleJust varAE n = do
+handleJust::Name->Exp->Exp->Q Exp
+handleJust n mFnE varAE = do
   cons <- lookupType n
-  blankBuildersMap <- buildAllBlankBuilders n
-  matches <- mapM (flip buildCaseMatch blankBuildersMap) cons
+  blankBuildersMap <- buildAllBlankBuilders n mFnE
+  let matchBuilder con = buildCaseMatch (nameBase n) mFnE con blankBuildersMap 
+  matches <- mapM matchBuilder cons
   return $ CaseE varAE matches
 
-buildAllBlankBuilders::Name->Q (M.Map ConId Exp) 
-buildAllBlankBuilders n = do
+buildAllBlankBuilders::Name->Exp->Q (M.Map ConId Exp) 
+buildAllBlankBuilders n mFnE = do
   cons <- lookupType n
-  cnames <- (fmap fst) <$> (mapM conNameAndTypes cons)
---  sumCE <- [e|\bldr->SumConstructor False bldr|] 
-  bldrs <- mapM buildBlankBuilder cons
---  let scs = map (\e->sumCE `AppE` e) bldrs
+  let first (x,_,_) = x
+  cnames <- (fmap first) <$> (mapM conNameAndTypes cons)
+  bldrs <- mapM (buildBlankBuilder (nameBase n) mFnE) cons
   let cids = map nameBase cnames
   return $ M.fromList (zip cids bldrs)
 
+sToE::String->Exp
+sToE = LitE . StringL
 
-buildCaseMatch::Con->M.Map ConId Exp->Q Match
-buildCaseMatch c builderMap = do
-  just <- [e|Just|] 
+buildCaseMatch::TypeName->Exp->Con->M.Map ConId Exp->Q Match
+buildCaseMatch typeN mFnE c builderMap = do
+  nothingE <- [e|Nothing|]
+  justE <- [e|Just|] 
   bldME <- [e|buildM|]
   applyE  <- [e|bApply|]
   injectE <- [e|bInject|]
-  setHasValE <- [e|bSetHasValue True|]
---  sumCE <- [e|\bldr->SumConstructor True bldr|]
   sumFE <- [e|\x -> bSum x|]
-  (n,tl) <- conNameAndTypes c
+  (n,tl,mFN) <- conNameAndTypes c
   ns <- mapM (\(ConT x)->newName $ toLower <$> (nameBase x)) tl
+  metaCE <- [e|Metadata|]
+  let metaE = metaCE `AppE` (sToE typeN) `AppE` (sToE (nameBase n)) `AppE` mFnE 
   let pats = VarP <$> ns
       vars = VarE <$> ns
-      bldrs = map (\x->bldME `AppE` (just `AppE` x)) vars -- [Exp], of type MFM m f a
-      conFE = injectE `AppE` (ConE n)
+      bldrs = case mFN of 
+        Nothing -> map (\x->bldME `AppE` nothingE `AppE` (justE `AppE` x)) vars -- [Exp], of type MFM m f a
+        Just fNames -> map (\(fn,vE)->bldME `AppE` (justE `AppE` (sToE $ nameBase fn)) `AppE` (justE `AppE` vE)) (zip fNames vars)
+      conFE = injectE `AppE` metaE `AppE` (ConE n)
       bldr = foldl (\e1 e2 -> applyE `AppE` e1 `AppE` e2) conFE bldrs
-      sc = setHasValE `AppE` sumCE `AppE` bldr
-      newMap = M.insert (nameBase n) sc builderMap
-      summedE = sumFE `AppE` (ListE $ map (\(cid,bldrE) -> TupE [LitE $ StringL cid,bldrE]) (M.toList newMap))
+      newMap = M.insert (nameBase n) bldr builderMap
+      summedE = sumFE `AppE` (ListE . snd . unzip . M.toList $ newMap)
   return $ Match (ConP n pats) (NormalB summedE) []
 
-buildBlankBuilder::Con->Q Exp
-buildBlankBuilder c = do
+buildBlankBuilder::TypeName->Exp->Con->Q Exp
+buildBlankBuilder typeN mFnE c = do
   nothingE <- [e|Nothing|]
+  justE <- [e|Just|]
   bldME <- [e|buildM|]
   applyE  <- [e|bApply|]
   injectE <- [e|bInject|]
-  (n,tl) <- conNameAndTypes c
-  let bldrs = replicate (length tl) (bldME `AppE` nothingE) -- [Exp], of type MFM m f a
-  bldrs' <- mapM [e|bSetHasValue False|] bldrs
-  let conFE = injectE `AppE` (ConE n)
-      foldBldrs = foldl (\e1 e2 -> applyE `AppE` e1 `AppE` e2) conFE bldrs' 
+  (n,tl,mFNs) <- conNameAndTypes c
+  let bldrs = case mFNs of
+        Nothing -> replicate (length tl) (bldME `AppE` nothingE `AppE` nothingE) -- [Exp], of type MFM m f a
+        Just fNames -> map (\n->bldME `AppE` (justE `AppE` (sToE $ nameBase n)) `AppE` nothingE) fNames
+  metaCE <- [e|Metadata|]
+  let metaE = metaCE `AppE` (sToE typeN) `AppE` (sToE $ nameBase n) `AppE` mFnE 
+  let conFE = injectE `AppE` metaE `AppE` (ConE n)
+      foldBldrs = foldl (\e1 e2 -> applyE `AppE` e1 `AppE` e2) conFE bldrs 
   return $ foldBldrs
 
 
